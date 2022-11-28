67366ed90004c857daa2519c5e639fd20dfe415c
Mark1708 <mark1708.work@gmail.com>
2022-11-29 02:10:01 +0300

    v1.0.0

--- src/main/java/example/com/App.java
+++ src/main/java/example/com/App.java
@@ -2,12 +2,9 @@ package example.com;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(new User("admin", "1234"));
     }
 }
